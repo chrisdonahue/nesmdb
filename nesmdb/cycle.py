909635e326022e51434aa383d15711a368d8be27
Chris Donahue <christopherdonahue@gmail.com>
2018-11-13 17:44:03 -0800

    Groundwork for event-based representation

--- nesmdb/cycle.py
+++ nesmdb/cycle.py
@@ -80,6 +80,19 @@ def _cycle_midi(ndr, rate):
   return ndr
 
 
+def _cycle_events(ndr, rate):
+  ndf = nesmdb.vgm.ndr_to_ndf(ndr)
+  rawsco = nesmdb.score.ndf_to_rawsco(ndf)
+  events = nesmdb.score.rawsco_to_events(rawsco)
+  rawsco = nesmdb.score.events_to_rawsco(events)
+  exprsco = nesmdb.score.rawsco_to_exprsco(rawsco)
+  exprsco = nesmdb.score.exprsco_downsample(exprsco, rate, False)
+  rawsco = nesmdb.score.exprsco_to_rawsco(exprsco)
+  ndf = nesmdb.score.rawsco_to_ndf(rawsco)
+  ndr = nesmdb.vgm.ndf_to_ndr(ndf)
+  return ndr
+
+
 def vgm_cycle(source_vgm, representation='ndf', keep_wav=False, **kwargs):
   # Cycle
   source_ndr = nesmdb.vgm.vgm_to_ndr(source_vgm)
@@ -99,6 +112,8 @@ def vgm_cycle(source_vgm, representation='ndf', keep_wav=False, **kwargs):
     cycle_ndr = _cycle_blndsco(source_ndr, rate=kwargs['score_rate'])
   elif representation == 'midi':
     cycle_ndr = _cycle_midi(source_ndr, rate=kwargs['score_rate'])
+  elif representation == 'events':
+    cycle_ndr = _cycle_events(source_ndr, rate=kwargs['score_rate'])
   else:
     raise NotImplementedError()
   cycle_vgm = nesmdb.vgm.ndr_to_vgm(cycle_ndr)
@@ -134,7 +149,7 @@ if __name__ == '__main__':
 
   parser = argparse.ArgumentParser()
 
-  parser.add_argument('representation', type=str, choices=['ndr', 'ndf', 'nlm', 'rawsco', 'exprsco', 'seprsco', 'blndsco', 'midi'])
+  parser.add_argument('representation', type=str, choices=['ndr', 'ndf', 'nlm', 'rawsco', 'exprsco', 'seprsco', 'blndsco', 'midi', 'events'])
   parser.add_argument('fps', type=str, nargs='+')
   parser.add_argument('--keep_vgm', action='store_true', dest='keep_vgm')
   parser.add_argument('--keep_wav', action='store_true', dest='keep_wav')
---
+++ nesmdb/score/events.py
@@ -0,0 +1,5 @@
+def rawsco_to_events(rawsco):
+  return rawsco
+
+def events_to_rawsco(events):
+  return events
